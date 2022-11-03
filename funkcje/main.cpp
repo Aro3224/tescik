c9b69f409ae1307b8258f1d2c060f8242c6a0536
student <student@it11-16.it.local>
2022-11-03 12:52:22 +0100

    wersja 1.4

--- funkcje/main.cpp
+++ funkcje/main.cpp
@@ -5,25 +5,55 @@ using namespace std;
 int main()
 {
     float y,a,b,x;
+    int wybor;
     cout << "Program oblicza miejsce zerowe funkcji" << endl;
-    cout<<"Podaj x"<<endl;
-    cin>>x;
-    cout<<"Podaj b"<<endl;
-    cin>>b;
-    cout<<"Podaj a"<<endl;
-    cin>>a;
-    if(a==0)
+
+    cout<<"ktorym sposobem chcesz obliczac miejsce zerowe funkcji liniowej?"<<endl;
+    cout<<"1. y=ax+b"<<endl;
+    cout<<"2. Ax+By+C=0"<<endl;
+    cin>>wybor;
+
+    switch(wybor)
     {
-        if(b==0)
+    case 1:
         {
-            cout<<"funkcja ma nieskonczonosc miejsc zerowych bo b=0"<<endl;
+                cout<<"Podaj x"<<endl;
+                cin>>x;
+                cout<<"Podaj b"<<endl;
+                cin>>b;
+                cout<<"Podaj a"<<endl;
+                cin>>a;
+
+                if(a==0)
+                {
+                    if(b==0)
+                    {
+                        cout<<"funkcja ma nieskonczonosc miejsc zerowych bo a=0 i b=0"<<endl;
+                    }
+                    else cout<<"funkcja nie ma miejsc zerowych bo a=0 i b="<<b<<endl;
+                }
+                else
+                {
+                    y=(a*x)+b;
+                    cout<<"miejsce zerowe wynosi: "<<y<<endl;
+                }
+                break;
         }
-        else cout<<"funkcja nie ma miejsc zerowych bo b="<<b<<endl;
+    case 2:
+        {
+            cout<<"Podaj A: ";
+            cin>>a;
+            if (a==0) cout<<"funkcja ma nieskonczonosc miejsc zerowych"<<endl;
+            else cout<<"miejsce zerowe wynosi: "<<a<<endl;
+            break;
+        }
+    default:
+        {
+            cout<<"podales zla liczbe"<<endl;
+        }
+
     }
-    else
-    {
-        y=(a*x)+b;
-        cout<<"miejsce zerowe wynosi: "<<y<<endl;
-    }
+
+
     return 0;
 }
