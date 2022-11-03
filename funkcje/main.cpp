1ff8707798a16fc17f5a59657673815ea71991cf
student <student@it11-16.it.local>
2022-11-03 12:20:28 +0100

    wersja 1.2

--- funkcje/main.cpp
+++ funkcje/main.cpp
@@ -6,18 +6,24 @@ int main()
 {
     float y,a,b,x;
     cout << "Program oblicza miejsce zerowe funkcji" << endl;
-    cout<<"Podaj a"<<endl;
-    cin>>a;
-    while(a==0)
-    {
-       cout<<"a nie moze byc 0, podaj inna liczbe"<<endl;
-       cin>>a;
-    }
-    cout<<"Podaj b"<<endl;
-    cin>>b;
     cout<<"Podaj x"<<endl;
     cin>>x;
-    y=(a*x)+b;
-    cout<<"miejsce zerowe wynosi: "<<y<<endl;
+    cout<<"Podaj b"<<endl;
+    cin>>b;
+    cout<<"Podaj a"<<endl;
+    cin>>a;
+    if(a==0)
+    {
+        if(b==0)
+        {
+            cout<<"funkcja ma nieskonczonosc miejsc zerowych bo b=0"<<endl;
+        }
+        else cout<<"funkcja nie ma miejsc zerowych bo b="<<b<<endl;
+    }
+    else
+    {
+        y=(a*x)+b;
+        cout<<"miejsce zerowe wynosi: "<<y<<endl;
+    }
     return 0;
 }
