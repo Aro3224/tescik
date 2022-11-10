f9ff668786d116a8541ad0181b860920e7f0d496
Konrad Szczurek <konradszcz1@onet.pl>
2022-11-10 12:56:59 +0100

    Wersja 3.0

--- funkcje/main.cpp
+++ funkcje/main.cpp
@@ -16,7 +16,7 @@ int main()
     {
     case 1:
         {
-            cout<<"ktorym sposobem chcesz obliczac miejsce zerowe funkcji liniowej?"<<endl;
+            cout<<"Ktorym sposobem chcesz obliczac miejsce zerowe funkcji liniowej?"<<endl;
             cout<<"1. y=ax+b"<<endl;
             cout<<"2. Ax+By+C=0"<<endl;
             cin>>wybor;
@@ -36,14 +36,14 @@ int main()
                         {
                             if(b==0)
                             {
-                                cout<<"funkcja ma nieskonczonosc miejsc zerowych bo a=0 i b=0"<<endl;
+                                cout<<"Funkcja ma nieskonczonosc miejsc zerowych bo a=0 i b=0"<<endl;
                             }
-                            else cout<<"funkcja nie ma miejsc zerowych bo a=0 i b="<<b<<endl;
+                            else cout<<"Funkcja nie ma miejsc zerowych bo a=0 i b="<<b<<endl;
                         }
                         else
                         {
                             y=(a*x)+b;
-                            cout<<"miejsce zerowe wynosi: "<<y<<endl;
+                            cout<<"Miejsce zerowe wynosi: "<<y<<endl;
                         }
                         break;
                 }
@@ -57,7 +57,7 @@ int main()
                 }
             default:
                 {
-                    cout<<"podales zla liczbe"<<endl;
+                    cout<<"Podales zla liczbe"<<endl;
                 }
 
             }
@@ -80,7 +80,7 @@ int main()
                         cin>>a;
                         if(a==0)
                         {
-                            cout<<"funkcja jest liniowa"<<endl;
+                            cout<<"Funkcja jest liniowa"<<endl;
                         }
 
                         else
@@ -90,7 +90,7 @@ int main()
                             cout<<"Podaj c"<<endl;
                             cin>>c;
 
-                            cout<<"twoja funkcja to: "<<a<<"x^2+"<<b<<"x+"<<c<<endl;
+                            cout<<"Twoja funkcja to: "<<a<<"x^2+"<<b<<"x+"<<c<<endl;
 
                             delta=(b*b)-(4*a*c);
 
@@ -100,14 +100,14 @@ int main()
                             {
                                 x1=(-b+sqrt(delta))/(2*a);
                                 x2=(-b-sqrt(delta))/(2*a);
-                                cout<<"miejsca zerowe wynosza: x1="<<x1<<" x2="<<x2<<endl;
+                                cout<<"Miejsca zerowe wynosza: x1="<<x1<<" x2="<<x2<<endl;
                             }
                             else if(delta==0)
                             {
                                 x=-b/(2*a);
-                                cout<<"miejsce zerowe wynosi: "<<x<<endl;
+                                cout<<"Miejsce zerowe wynosi: "<<x<<endl;
                             }
-                                else cout<<"funkcja nie ma miejsc zerowych"<<endl;
+                                else cout<<"Funkcja nie ma miejsc zerowych"<<endl;
                         }
                         break;
                     }
@@ -158,7 +158,7 @@ int main()
 
     default:
         {
-            cout<<"podales zla liczbe"<<endl;
+            cout<<"Podales zla liczbe"<<endl;
         }
 
     }
